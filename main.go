73dfaa1e6819f267019811299a345616c1caabc1
N N Advith <nnadvith08042001@gmail.com>
2025-04-22 00:24:33 +0530

    eh

--- main.go
+++ main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/nn-advith/smolurl/kvmodule"
+	logger "github.com/nn-advith/smolurl/logger"
 )
 
 func main() {
-	fmt.Println("Hello World.")
+
+	// initialise logger
+	// initialise db connection
+	// start the web server
+	// listen for requests
+
+	err := logger.InitLogger(true, true)
+	alog := logger.GlobalLogger
+	if err != nil {
+		fmt.Printf("initlogger error - %v ", err)
+	}
+	defer logger.GlobalLogger.Close()
+
+	kvmodule.InitialiseDB("COUCHBASE")
+
+	alog.Info("This is a test after global loggeer refactor")
 }

63353a871d2b1cf61949b53f7cca3cd779c5a903
N N Advith <nnadvith08042001@gmail.com>
2025-04-23 22:27:05 +0530

    driver code

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/nn-advith/smolurl/kvmodule"
+	"github.com/nn-advith/smolurl/kvmodule/datamodel"
 	logger "github.com/nn-advith/smolurl/logger"
 )
 
@@ -14,6 +15,11 @@ func main() {
 	// start the web server
 	// listen for requests
 
+	//variables
+	DBTYPE := "COUCHBASE"
+	COLLECTION := "smolurl"
+
+	//initialise logger with both stdout and logfile
 	err := logger.InitLogger(true, true)
 	alog := logger.GlobalLogger
 	if err != nil {
@@ -21,7 +27,26 @@ func main() {
 	}
 	defer logger.GlobalLogger.Close()
 
-	kvmodule.InitialiseDB("COUCHBASE")
-
+	//db init and defer disconnect
+	dbinstance := kvmodule.InitialiseDB(DBTYPE, COLLECTION)
+	defer func() {
+		if err := dbinstance.Disconnect(); err != nil {
+			logger.GlobalLogger.Fatal(err)
+		}
+	}()
 	alog.Info("This is a test after global loggeer refactor")
+
+	alog.Info("Trying insert")
+
+	var newhash datamodel.UrlEntry = datamodel.UrlEntry{
+		ID:      "SOMEHASH",
+		LongURL: "https://nnadvith.netlify.app",
+		Created: 45545435,
+		TTL:     60000,
+	}
+
+	err = dbinstance.Insert(COLLECTION, newhash)
+	if err != nil {
+		logger.GlobalLogger.Error("MAIN: error during insert: ", err)
+	}
 }

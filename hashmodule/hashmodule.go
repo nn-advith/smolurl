39ffa9f20f6254ba1281483f5ae3dc0e996a1ada
N N Advith <nnadvith08042001@gmail.com>
2025-05-09 21:19:36 +0530

    salted hash generator

--- hashmodule/hashmodule.go
+++ hashmodule/hashmodule.go
@@ -1,1 +1,15 @@
 package hashmodule
+
+import (
+	"crypto/sha256"
+	"encoding/base64"
+	"strconv"
+)
+
+func GenerateHash(url string, attempts int) string {
+	salted := url + strconv.Itoa(attempts)
+	h := sha256.Sum256([]byte(salted))
+	slug := base64.URLEncoding.EncodeToString(h[:8])
+	// logger.GlobalLogger.Info(h, slug)
+	return slug
+}
